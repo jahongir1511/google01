b27187f825690379ffbe7e5ec4a588bb353bf8de
jahongir1511 <159403888+jahongir1511@users.noreply.github.com>
2024-07-15 20:55:07 +0500

    css

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    image = models.ImageField(upload_to='users_image/', blank=True, null=True, default='default_img/user_img.png')
+
+    class Meta:
+        db_table = 'customuser'
+
+    def __str__(self):
+        return self.username
